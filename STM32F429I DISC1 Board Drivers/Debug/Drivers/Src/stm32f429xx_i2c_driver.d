2db44990170a7b9af029aa360ce187432bf6978c
Ibrahim Okur <ibrahim.e.okur@gmail.com>
2025-01-06 00:41:04 +0100

    New files were added, and folder was changed.

---
+++ STM32F429I DISC1 Board Drivers/Debug/Drivers/Src/stm32f429xx_i2c_driver.d
@@ -0,0 +1,14 @@
+Drivers/Src/stm32f429xx_i2c_driver.o: \
+ ../Drivers/Src/stm32f429xx_i2c_driver.c \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_gpio_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_spi_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_rcc_driver.h
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_gpio_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_spi_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_rcc_driver.h:
---
+++ STM32F429I DISC1 Board Drivers/Debug/Src/010i2c_master_tx_testing.d
@@ -0,0 +1,19 @@
+Src/010i2c_master_tx_testing.o: ../Src/010i2c_master_tx_testing.c \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_gpio_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_spi_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_rcc_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_gpio_driver.h \
+ D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_rcc_driver.h
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_gpio_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_spi_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_rcc_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_i2c_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_gpio_driver.h:
+D:/STM32F429I-DISC1\ Repository/STM32F429I-DISC1\ Local\ Project/STM32F429I\ DISC1\ Board\ Drivers/Drivers/Inc/stm32f429xx_rcc_driver.h:
